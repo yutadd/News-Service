bfd4eda281cb03f3e15a634e9568b29257e99e1b
yutadd <yutadd@host.docker.internal>
2022-06-09 00:00:27 +0900

    edited_HTML

---
+++ src/test/java/news/com/yutadd/MyController.java
@@ -0,0 +1,12 @@
+package news.com.yutadd;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+	@RequestMapping("/")
+	public String top() {
+		return "top";
+	}
+}
