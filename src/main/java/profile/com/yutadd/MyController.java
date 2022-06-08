db71754b905c96fbb56fccd50807245b7e428f2c
yutadd <yutadd@host.docker.internal>
2022-06-09 00:48:32 +0900

    base completed

---
+++ src/main/java/profile/com/yutadd/MyController.java
@@ -0,0 +1,24 @@
+package profile.com.yutadd;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+	@RequestMapping({"/","/top"})
+	public String top() {
+		return "top";
+	}
+	@RequestMapping({"/otoi"})
+	public String otoi() {
+		return "otoi";
+	}
+	@RequestMapping({"/live"})
+	public String live() {
+		return "live";
+	}
+	@RequestMapping({"/travel"})
+	public String travel() {
+		return "/travel";
+	}
+}
