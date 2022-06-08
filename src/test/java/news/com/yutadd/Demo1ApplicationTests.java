145bb677cfbe4a579997a26bd205ef56b9b75b12
yutadd <yutadd@host.docker.internal>
2022-06-08 21:24:01 +0900

    init

---
+++ src/test/java/news/com/yutadd/Demo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package news.com.yutadd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
